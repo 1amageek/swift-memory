ea204119c3ca1bede179c509fc73d66117d17527
1amageek <tmy0x3@icloud.com>
2026-04-22 20:43:04 +0900

    Bump database-kit and database-framework to 26.0422.0
    - database-kit: from 26.0418.0 -> 26.0422.0
    - database-framework: from 26.0421.3 -> 26.0422.0

--- Package.swift
+++ Package.swift
@@ -10,10 +10,10 @@ let package = Package(
         .library(name: "SwiftMemory", targets: ["SwiftMemory"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/1amageek/database-kit.git", from: "26.0418.0"),
+        .package(url: "https://github.com/1amageek/database-kit.git", from: "26.0422.0"),
         .package(
             url: "https://github.com/1amageek/database-framework.git",
-            from: "26.0421.3",
+            from: "26.0422.0",
             traits: ["SQLite"]
         ),
         .package(
